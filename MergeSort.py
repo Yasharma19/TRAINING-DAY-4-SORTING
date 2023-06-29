31efbceb22244ce0bb1924b1639fc7dd8560671f
Yash Sharma <yash304sharma@gmail.com>
2023-06-30 01:28:43 +0530

    Create MergeSort.py

---
+++ MergeSort.py
@@ -0,0 +1,45 @@
+def mergeSort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left = mergeSort(arr[:mid])
+    right = mergeSort(arr[mid:])
+
+    return merge(left, right)
+
+def merge(left, right):
+    merged = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            merged.append(left[i])
+            i += 1
+        else:
+            merged.append(right[j])
+            j += 1
+
+    while i < len(left):
+        merged.append(left[i])
+        i += 1
+
+    while j < len(right):
+        merged.append(right[j])
+        j += 1
+
+    return merged
+
+# Read the number of elements
+n = int(input())
+
+# Read the elements of the array
+arr = list(map(int, input().split()))
+
+# Sort the array using merge sort
+sorted_arr = mergeSort(arr)
+
+# Print the sorted array
+for num in sorted_arr:
+    print(num, end=' ')
+print()
